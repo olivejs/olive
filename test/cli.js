aedadbfecc7369722a9cbfe52227aae6ef448f46
Mohammad Islam <reefat@gmail.com>
2015-07-19 01:41:19 -0400

    Use `commander` to parse CLI commands/options

--- test/cli.js
+++ test/cli.js
@@ -1,34 +1,46 @@
-var path = require('path');
-var pkg = require('../package.json');
 var chai = require('chai');
 var expect = chai.expect;
 
+var pkg = require('../package.json');
+var path = require('path');
 var spawnSync = require('child_process').spawnSync;
 var execFileSync = require('child_process').execFileSync;
 
-describe('cli', function() {
+var cli = path.join(__dirname, '../lib/cli.js');
 
-  it('should return 1 when no command is supplied', function() {
-    var exitCode = spawnSync('node', [path.join(__dirname, '../lib/cli')]).status;
+describe('ginger', function() {
+
+  it('should return with exit code 1 when no command is supplied', function() {
+    var exitCode = spawnSync('node', [cli]).status;
     var expected = 1;
     expect(exitCode).to.be.equal(expected);
   });
 
-  it('should return 2 when an invalid command is supplied', function() {
-    var exitCode = spawnSync('node', [path.join(__dirname, '../lib/cli'), 'invalidcommand']).status;
+  it('should return with exit code 2 when an invalid command is supplied', function() {
+    var exitCode = spawnSync('node', [cli, 'invalidcommand']).status;
     var expected = 2;
     expect(exitCode).to.be.equal(expected);
   });
 
-  it('should output version when passed -v or --version option', function() {
+  it('should output version when passed -V or --version option', function() {
     var version;
     var expected = pkg.version;
 
-    version = execFileSync('node', [path.join(__dirname, '../lib/cli'), '-v'], { encoding: 'utf8' }).trim();
+    version = execFileSync('node', [cli, '-V'], { encoding: 'utf8' }).trim();
     expect(version).to.be.equal(expected);
 
-    version = execFileSync('node', [path.join(__dirname, '../lib/cli'), '--version'], { encoding: 'utf8' }).trim();
+    version = execFileSync('node', [cli, '--version'], { encoding: 'utf8' }).trim();
     expect(version).to.be.equal(expected);
+  });
+
+});
+
+describe('ginger new', function() {
+
+  it('should return with exit code 1 when missing seed argument', function() {
+    var exitCode = spawnSync('node', [cli, 'new']).status;
+    var expected = 1;
+    expect(exitCode).to.be.equal(expected);
   });
 
 });
